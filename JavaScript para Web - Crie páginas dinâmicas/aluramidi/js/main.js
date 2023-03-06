df60916424ce28f43249894e3d86719baa9b5078
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-03-06 19:00:50 -0300

    unkeydown

--- JavaScript para Web - Crie páginas dinâmicas/aluramidi/js/main.js
+++ JavaScript para Web - Crie páginas dinâmicas/aluramidi/js/main.js
@@ -1,19 +1,36 @@
-function tocarSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//Lógica de "enquanto" (while)
+//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-
     const instrumento = tecla.classList[1];
-
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
-        tocarSom(idAudio);
+        tocaSom(idAudio);
     }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+
 }
